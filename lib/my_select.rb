28c44f43b7c9a5c1f524697edd759ac0b6d253b5
Daniel Sullivan <dansullivan19@gmail.com>
2019-04-08 16:39:35 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  collection = []
+  i = 0 
+  while i < array.length
+    if true
+    collection.push(yield(array[i]))
+    else
+    i += 1 
+    end
+  end
+  collection
 end
+
+
+names = ["Bo", "Cal", "Dee"]
+
+my_select(names) { |name| puts "#{name}" }
